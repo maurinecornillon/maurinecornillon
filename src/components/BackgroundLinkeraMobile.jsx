7060faa6b5e5ee3906a741311079d40c82be2686
maurinecornillon <102037156+maurinecornillon@users.noreply.github.com>
2024-10-08 01:16:07 +0200

    test img repsonsive

--- src/components/BackgroundLinkeraMobile.jsx
+++ src/components/BackgroundLinkeraMobile.jsx
@@ -6,15 +6,11 @@ const BackgroundLinkeraWMobile = () => {
   return (
     <>
       {/* Section avec l'effet de parallaxe en dessous du titre */}
-      <div
-        className="bg-fixed h-screen border-2 border-rose-500"
-        style={{
-          backgroundImage: `url(${mockup})`,
-          backgroundPosition: "center top 40%",
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat", // Empêche la répétition de l'image
-        }}
-      ></div>
+      <img
+        src={mockup} // URL de ton image
+        alt="Mockup Linkera"
+        className="w-full h-full object-cover"
+      />
     </>
   );
 };
